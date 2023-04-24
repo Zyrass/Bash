73c1853af24b36048b784100c83913236a788c3f
Alain Guillon <a.guillon@it-students.fr>
2023-04-24 09:22:07 +0200

    add a exo 4 et 5

---
+++ mohamed/exo4.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Vérification du nombre de paramètres
+if [ $# -ne 3 ]; then
+    echo -e "\n Comment l'utiliser: $0 <nombre1> <opérateur> <nombre2>"
+    exit 1
+fi
+
+# Récupération des paramètres
+num1=$1
+op=$2
+num2=$3
+
+# Vérification de l'opérateur
+case $op in
++) result=$(($num1 + $num2)) ;;
+-) result=$(($num1 - $num2)) ;;
+x) result=$(($num1 * $num2)) ;;
+/) result=$(($num1 / $num2)) ;;
+*)
+    echo "Opérateur non supporté sur ce programme"
+    exit 1
+    ;;
+esac
+
+# Affichage du résultat
+echo "$num1 $op $num2 = $result"
---
+++ mohamed/exo5.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Vérifie si un argument a été donné en paramètre
+if [[ -z $1 ]]; then
+    # Si non, demande à l'utilisateur de saisir un nombre
+    read -p "Entrez un nombre : " n
+else
+    # Si oui, utilise le premier paramètre comme nombre à factoriser
+    n=$1
+fi
+
+# Initialise la variable fact à 1
+fact=1
+
+# Boucle for pour calculer la factorielle de n
+for ((i = 1; i <= n; i++)); do
+    # Multiplie fact par i à chaque itération
+    fact=$((fact * i))
+done
+
+# Affiche le résultat
+echo "La factorielle de $n est : $fact"
